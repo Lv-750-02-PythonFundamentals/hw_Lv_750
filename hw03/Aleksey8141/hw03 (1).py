08dbe2591213e04f06a8de3d5e4dfbba9c33587f
VadimMarkov1 <121629593+VadimMarkov1@users.noreply.github.com>
2023-02-17 15:44:06 +0200

    Revert "Vadim markov1"

--- hw03/Aleksey8141/hw03 (1).py
+++ hw03/Aleksey8141/hw03 (1).py
@@ -1,9 +1,13 @@
-Beautiful is better than ugly.
+'''You need to write Python's philosophy in the string format:
+- find separately the number of occurrences of the words
+- "better", "never" and "is" in the given line
+- you need to display all text in uppercase (all letters in uppercase)
+- replace all occurrences of the symbol “i” with “&”
+'''
+
+a = '''Beautiful is better than ugly.
 Explicit is better than implicit.
-<<<<<<< HEAD
-=======
 Simple is better than complex.
->>>>>>> 1bd5031 (Revert "Vadim markov1")
 Complex is better than complicated.
 Flat is better than nested.
 Sparse is better than dense.
@@ -19,4 +23,13 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
+Namespaces are one honking great idea -- let's do more of those!'''
+
+
+print(a.upper())
+
+print('Кількість "better" =', a.count('better'))
+print('Кількість "never" =', a.count('never'))
+print('Кількість "is" =', a.count('is'))
+
+print(a.replace('i', '&'))
