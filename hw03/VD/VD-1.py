aa0dafbac6fa63dfa1e796464b0722526d473e5e
Volodymyr Dukhanin <delta.dukhanin@gmail.com>
2023-02-17 17:49:53 +0100

    hw03 TASK 1

--- hw03/VD/VD-1.py
+++ hw03/VD/VD-1.py
@@ -1,4 +1,4 @@
-# You need to write Python's philosophy in the string format
+# You need to write Python's philosophy in the string format:
 python_philosofy = """Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -20,8 +20,11 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
 
 # - find separately the number of occurrences of the words - "better", "never" and "is" in the given line
-python_philosofy.find("better", "never", "is")
+print(f'better counted {python_philosofy.count("better")} times', f'never counted {python_philosofy.count("never")} times', f'is counted {python_philosofy.count("is")} times', sep="\n", end="\n\n")
 
-# you need to display all text in uppercase (all letters in uppercase)
+# - you need to display all text in uppercase (all letters in uppercase)
+print(python_philosofy.upper(), end="\n\n")
 
-python_philosofy.upper
+# - replace all occurrences of the symbol “i” with “&”
+print(python_philosofy.replace("i", "&"))
+
