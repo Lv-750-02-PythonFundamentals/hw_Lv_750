d9fd7d3ddbd8680120172b15346b8be72c8d563f
Aleksey8141 <alekseyoleksa@gmail.com>
2023-02-19 12:00:34 +0200

    add hw04 (factorial)

---
+++ hw04/Aleksey8141/Factorial.py
@@ -0,0 +1,15 @@
+'''Write a script that will calculate the
+factorial of the entered number without using
+recursion.'''
+
+from math import*
+n = int(input())
+print(factorial(n))
+
+# n=int(input())
+# def factorial(n):
+#     factorial = 1
+#     for i in range(n):
+#         factorial*=i+1
+#     return factorial
+# print(factorial(n))
