33be9670add0d941b36f055d3940ac4705344530
OleksandrLypchei <oleksandrlypchei@gmail.com>
2023-02-20 09:45:25 +0200

    Adding if statements to escape input errors

--- hw04/OleksandrLypchei/factorial.py
+++ hw04/OleksandrLypchei/factorial.py
@@ -1,5 +1,15 @@
 import math
 
 
-num = int(input("Enter number: "))
-print(f"Factorial of your number is {math.factorial(num)}")
+num = input("Enter number: ")
+
+# Checking if number is float
+if not num.isdigit():
+    print("Your number is not integer. Try integer number!")
+else:
+    num = int(num)
+    # Checking if number is negative
+    if num < 0:
+        print("Your number is negative! Try positive number!")
+    else:
+        print(f"Factorial of your number is {math.factorial(num)}")
