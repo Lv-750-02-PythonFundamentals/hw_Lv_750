fba825ee0fb3ed7fca38cc2fe92d025c99c5f3f5
Aleksey8141 <alekseyoleksa@gmail.com>
2023-02-23 21:42:49 +0200

    add Aleksey8141 hw05

--- hw05/Aleksey8141/Practical task #3.py
+++ hw05/Aleksey8141/Practical task #3.py
@@ -2,11 +2,13 @@
 (Hint: use the break statement)'''
 
 list = [2, 4, 6, 7, 8, 10]
+odd = False
 
 for i in list:
-        if i % 2 == 0:
-            continue
-        elif i % 2 != 0:
-            print('The list contains odd numbers')
+        if i % 2 != 0:
+            odd = True
             break
-
+if odd:
+    print("The list contains odd numbers")
+else:
+    print("The list does not contain odd numbers")
