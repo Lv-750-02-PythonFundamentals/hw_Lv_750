b3fbd867df97273f2c41e45b39b48a25d6647a7f
Aleksey8141 <alekseyoleksa@gmail.com>
2023-02-23 21:36:44 +0200

    add Aleksey8141 hw05

--- hw05/Aleksey8141/Practical task #3.py
+++ hw05/Aleksey8141/Practical task #3.py
@@ -3,8 +3,10 @@
 
 list = [2, 4, 6, 7, 8, 10]
 
-
 for i in list:
-        if i % 2 != 0:
-            print('The odd numbder is', i)
+        if i % 2 == 0:
+            continue
+        elif i % 2 != 0:
+            print('The list contains odd numbers')
             break
+
