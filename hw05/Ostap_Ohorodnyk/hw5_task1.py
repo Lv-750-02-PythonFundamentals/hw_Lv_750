e3d5b4948f78a427f467fe1a1f7cb1d01bae1255
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-02-24 15:44:00 +0200

    made some changes to make a pull request

--- hw05/Ostap_Ohorodnyk/hw5_task1.py
+++ hw05/Ostap_Ohorodnyk/hw5_task1.py
@@ -1,4 +1,4 @@
-number_for_list = int(input("enter a number for the length of the list: "))
+number_for_list = int(input("enter a number for the length of the list:  "))
 my_list = [*range(1, number_for_list+1)]
 float_list = []
 
