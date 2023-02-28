e1ce3de9f0c1a99204001294e7eef4fe38363877
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-02-28 18:43:00 +0200

    hw6 practice task1

---
+++ hw06/Ostap_Ohorodnyk/hw6_practice_task1.py
@@ -0,0 +1,7 @@
+n = input("Enter a list of numbers, separate the numbers with a space :").split()
+result = []
+
+for num in n: result.append(int(num))
+
+print(f"Еhe maximum number from the list: {max(result)};\n"
+      f"The minimum number from the list: {min(result)}.")
