19b761a4c5d66fd3690b46a0de4e817474df294d
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-02-28 17:53:20 +0200

    created a new branch

--- hw06/Ostap_Ohorodnyk/hw6_task2.py
+++ hw06/Ostap_Ohorodnyk/hw6_task2.py
@@ -2,7 +2,7 @@ password = input("Enter your password: ")
 valid_password = "First"
 
 while password != valid_password:
-    print("The password is not correct")
+    print("The password is not correct!")
     password = input("Enter your password: ")
 
 print("The password is correct")
