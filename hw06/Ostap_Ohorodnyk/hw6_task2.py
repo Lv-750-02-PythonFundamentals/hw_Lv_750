5aa8aeacd7fb6ff179802f9acf909ee02b7d9d3f
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-02-28 17:48:18 +0200

    hw6 task2

---
+++ hw06/Ostap_Ohorodnyk/hw6_task2.py
@@ -0,0 +1,10 @@
+password = input("Enter your password: ")
+valid_password = "First"
+
+while password != valid_password:
+    print("The password is not correct")
+    password = input("Enter your password: ")
+
+print("The password is correct")
+
+
