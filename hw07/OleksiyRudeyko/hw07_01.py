61c793762e003850cdabac435b4f17e6983bfd77
Oleksiy Rudeyko <oleksiy.rudeyko@gmail.com>
2023-03-03 14:07:07 +0200

    task 01 with Docstring

--- hw07/OleksiyRudeyko/hw07_01.py
+++ hw07/OleksiyRudeyko/hw07_01.py
@@ -1,4 +1,18 @@
 def largest_of_two(num1, num2):
+    """
+    Returns the largest number of two numbers.
+    
+    Parameters:
+    num1: the first number.
+    num2: the second number.
+    
+    Checks:
+    if num1 is larger than num2
+
+    Returns:
+    formatted string "num1 is larger than num2" if the condition is True
+    otherwise returns formatted string "num2 is larger than num1"
+    """
     if num1 > num2:
         return f"{num1} is larger than {num2}"
     return f"{num2} is larger than {num1}"
