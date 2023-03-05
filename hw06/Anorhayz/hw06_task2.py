cfd0a36e840eb0e97c7ada174ef160a94d17fdac
Pavlo Shymanskyi <shymanskyi.p.289@gmail.com>
2023-03-05 10:30:53 +0200

    Modify hw06_task2

--- hw06/Anorhayz/hw06_task2.py
+++ hw06/Anorhayz/hw06_task2.py
@@ -1,7 +1,8 @@
 user_login = input("Enter your login: ")
+correct_login = "First"
 
-while user_login == "First":
-    print("Login is confirmed")
-    break
-else:
+while user_login != correct_login:
     print("Incorrect login, try again")
+    user_login = input("Please enter your login: ")
+else:
+    print("Login is confirmed")
