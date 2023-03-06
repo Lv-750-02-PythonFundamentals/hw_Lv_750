d8a464aba89bb0731ebaf1090ed3736358498754
Luzhanskiy7 <107493221+Luzhanskiy7@users.noreply.github.com>
2023-03-06 11:39:04 +0200

    task 7_1 is done

---
+++ hw07/Luzhanskiy7/task7_1.py
@@ -0,0 +1,12 @@
+def the_largest_num(a, b) -> int:
+    """This function returns the larger of the two values"""
+
+    if a > b:
+        print(a)
+    elif b > a:
+        print(b)
+    elif a == b:
+        print(a)
+
+
+
