5d6436b71df0e83c4edb289cc471e4c19aa80f15
Vadym Markov <trezub123456@gmail.com>
2023-03-08 14:31:28 +0200

    hw08

---
+++ hw08/VadimMarkov1/task1/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import *
+from .admin import *
+
+print(dir())
+
+__all__ = ['create_user', 'create_admin']
+
+
---
+++ hw08/VadimMarkov1/task3/area_calculator.py
@@ -0,0 +1,16 @@
+from math import pow, pi
+
+
+def calculate_rectangle_area(a, b):
+    """The  function calculates the area of rectangle"""
+    return a*b
+
+
+def calculate_triangle_area(a, h):
+    """The  function calculates the area of triangle"""
+    return 0.5*a*h
+
+
+def calculate_circle_area(r):
+    """The  function calculates the area of circle"""
+    return pi*pow(r)
