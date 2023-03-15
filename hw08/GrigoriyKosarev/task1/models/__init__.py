420aa2c6e88af6f6332f07cefc69e302cfd564fd
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2023-03-15 12:25:38 +0200

    ready. please check

--- hw08/GrigoriyKosarev/task1/models/__init__.py
+++ hw08/GrigoriyKosarev/task1/models/__init__.py
@@ -1,6 +1,6 @@
 
-from hw08.GrigoriyKosarev.task1.models.admin import *
-from hw08.GrigoriyKosarev.task1.models.user import *
+from .admin import *
+from .user import *
 
 __all__ = ["create_admin", "create_user"]
 
--- hw08/GrigoriyKosarev/task1/utils/__init__.py
+++ hw08/GrigoriyKosarev/task1/utils/__init__.py
@@ -1,5 +1,5 @@
 
-from hw08.GrigoriyKosarev.task1.utils.logger import *
-from hw08.GrigoriyKosarev.task1.utils.formatter import *
+from .logger import *
+from .formatter import *
 
 __all__ = ["format_string", "log_in_file"]
