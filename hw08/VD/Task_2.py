34741983760fa0e7bc3729654968b33513291cf2
Volodymyr Dukhanin <delta.dukhanin@gmail.com>
2023-03-17 00:01:56 +0100

    Task 2 added

--- hw08/VD/Task_2.py
+++ hw08/VD/Task_2.py
@@ -1,2 +1,20 @@
-"""To be done..."""
+"""Write a Python program to check the validity of a password (input from users).
+Validation :
+At least 1 letter between [a-z] and 1 letter between [A-Z].
+At least 1 number between [0-9].
+At least 1 character from [$#@].
+Minimum length 6 characters.
+Maximum length 16 characters.
+"""
+
+import re
+
+while True:
+    password = input("Please enter the password: ")
+
+    if re.search(r"^.{6,16}$", password) and re.search(r".*[a-z]", password) and re.search(r".*[A-Z]", password) and re.search(r".*[@#$]", password):
+        print("Password is OK")
+        break
+    else:
+        print("Password is not OK, try another")
 
