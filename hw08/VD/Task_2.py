685a761a4ae4e85cf10c95db84c0f8e536ebda99
Volodymyr Dukhanin <delta.dukhanin@gmail.com>
2023-03-17 00:46:19 +0100

    Task 2 two possible solutions added

--- hw08/VD/Task_2.py
+++ hw08/VD/Task_2.py
@@ -12,9 +12,17 @@ import re
 while True:
     password = input("Please enter the password: ")
 
-    if re.search(r"^.{6,16}$", password) and re.search(r".*[a-z]", password) and re.search(r".*[A-Z]", password) and re.search(r".*[@#$]", password):
+    # if re.search(r"^.{6,16}$", password) and re.search(r".*[a-z]", password) and re.search(r".*[A-Z]", password) and re.search(r".*[@#$]", password):
+    #     print("Password is OK")
+    #     break
+    # else:
+    #     print("Password is not OK, try another")
+
+# 2nd solution using Lookahead Assertions
+### check condition if omewhere in string presented [a-z], [A-Z], \d, [@#$] and limit search of all [a-zA-Z\d@#$] from 6 to 16 from ^ start till $ end
+
+    if re.search(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$#@])[a-zA-Z\d@#$]{6,16}$", password):
         print("Password is OK")
         break
     else:
         print("Password is not OK, try another")
-
