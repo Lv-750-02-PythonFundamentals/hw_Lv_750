2eccf711d9b3e7f9b7bb02755997e3d446b1aa1a
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-03-21 22:01:28 +0200

    homework 10 task1

---
+++ hw10/Ostap_Ohorodnuk/Task1.py
@@ -0,0 +1,22 @@
+""""A polygon class and a rectangle class"""
+
+
+class Polygon:
+    """polygon class"""
+    pass
+
+
+class Rectangle(Polygon):
+    """rectangle class"""
+    def __init__(self, a=1, b=1):
+        self.a = a
+        self.b = b
+
+    def square(self):
+        self.a = int(input("enter side a: "))
+        self.b = int(input("enter side b: "))
+        return self.a * self.b
+
+square_of_rectangle = Rectangle()
+
+print(square_of_rectangle.square())
