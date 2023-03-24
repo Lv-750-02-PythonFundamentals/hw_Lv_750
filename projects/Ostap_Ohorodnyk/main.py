dbea59a8981d8cfb30f894c46343fbfdee188226
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-03-24 17:40:39 +0200

    made syntax corrections

--- projects/Ostap_Ohorodnyk/main.py
+++ projects/Ostap_Ohorodnyk/main.py
@@ -1,3 +1,4 @@
+"""program to do list"""
 from tkinter import *
 from tkinter.font import Font
 
@@ -11,8 +12,7 @@ root.configure(bg="light gray")
 # Define font
 my_font = Font(family="Purisa",
                size=20,
-               weight="bold",
-               )
+               weight="bold",)
 
 # Creat frame
 my_frame=Frame(root)
@@ -29,7 +29,8 @@ my_list = Listbox(my_frame,
                   highlightthickness=0,
                   selectbackground="#a6a6a6",
                   activestyle="none")
-my_list.pack(side=LEFT, fill=BOTH, expand=False)
+
+my_list.pack(side=LEFT, fill=BOTH, )
 # start list
 
 stuff = ["write your tasks"]
@@ -47,6 +48,7 @@ my_scrollbar.config(command=my_list.yview)
 my_entry = Entry(root, font=("Purisa", 20), width=26)
 my_entry.pack(pady=20)
 # buttons
+
 button_frame = Frame(root)
 button_frame.pack(pady=20)
 # button function
@@ -54,11 +56,13 @@ button_frame.pack(pady=20)
 def add_item():
     my_list.insert(END, my_entry.get())
     my_entry.delete(0,END)
+
 def cross_off_item():
     my_list.itemconfig(
         my_list.curselection(), fg="#FFFAFA"
     )
     my_list.selection_clear(0,END)
+
 def uncross_off_item():
     my_list.itemconfig(
         my_list.curselection(), fg="#464646"
@@ -73,17 +77,15 @@ def delete_crossed():
         else:
             count += 1
 
-
 add_button = Button(button_frame, text="Add task", command=add_item)
 cross_off_button = Button(button_frame, text="Cross task", command=cross_off_item)
 uncross_off_button = Button(button_frame, text="Uncross tsk", command=uncross_off_item)
 delete_crossed_button = Button(button_frame, text="Delete crossed", command=delete_crossed)
 
-
 add_button.grid(row=0, column=0, padx=10)
 cross_off_button.grid(row=0, column=1, padx=10)
 uncross_off_button.grid(row=0, column=2, )
-delete_crossed_button.grid(row=0, column=3,padx=10)
+delete_crossed_button.grid(row=0, column=3, padx=10)
 
 
 root.mainloop()
