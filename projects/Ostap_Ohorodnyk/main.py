c1548bba2f10451d8afc5b6e3094025c1f63c792
Liubomyr Halamaha <galamagal86@gmail.com>
2023-03-25 10:06:17 +0200

    added lesson14

--- projects/Ostap_Ohorodnyk/main.py
+++ projects/Ostap_Ohorodnyk/main.py
@@ -1,10 +1,9 @@
 from tkinter import *
 from tkinter.font import Font
 
-
 root = Tk()
 root.title("My ToDO list!")
-bg="light gray"
+bg = "light red"
 root.geometry("570x500")
 root.configure(bg="light gray")
 
@@ -15,7 +14,7 @@ my_font = Font(family="Purisa",
                )
 
 # Creat frame
-my_frame=Frame(root)
+my_frame = Frame(root)
 my_frame.pack(pady=10)
 
 # Create list box
@@ -37,7 +36,7 @@ stuff = ["write your tasks"]
 for item in stuff:
     my_list.insert(END, item)
 
-#ADD scrollbar
+# ADD scrollbar
 my_scrollbar = Scrollbar(my_frame)
 my_scrollbar.pack(side=RIGHT, fill=BOTH)
 my_list.config(yscrollcommand=my_scrollbar.set)
@@ -49,21 +48,28 @@ my_entry.pack(pady=20)
 # buttons
 button_frame = Frame(root)
 button_frame.pack(pady=20)
+
+
 # button function
 
 def add_item():
     my_list.insert(END, my_entry.get())
-    my_entry.delete(0,END)
+    my_entry.delete(0, END)
+
+
 def cross_off_item():
     my_list.itemconfig(
         my_list.curselection(), fg="#FFFAFA"
     )
-    my_list.selection_clear(0,END)
+    my_list.selection_clear(0, END)
+
+
 def uncross_off_item():
     my_list.itemconfig(
         my_list.curselection(), fg="#464646"
     )
     my_list.selection_clear(0, END)
+
 
 def delete_crossed():
     count = 0
@@ -79,11 +85,9 @@ cross_off_button = Button(button_frame, text="Cross task", command=cross_off_ite
 uncross_off_button = Button(button_frame, text="Uncross tsk", command=uncross_off_item)
 delete_crossed_button = Button(button_frame, text="Delete crossed", command=delete_crossed)
 
-
 add_button.grid(row=0, column=0, padx=10)
 cross_off_button.grid(row=0, column=1, padx=10)
 uncross_off_button.grid(row=0, column=2, )
-delete_crossed_button.grid(row=0, column=3,padx=10)
-
+delete_crossed_button.grid(row=0, column=3, padx=10)
 
 root.mainloop()
